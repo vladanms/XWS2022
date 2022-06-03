be290abf80dff0636e8e66e67bdfe30d70ef9482
VladanS <49107047+vladanms@users.noreply.github.com>
2022-06-03 19:22:39 +0200

    Base

--- XWS/data/comment.go
+++ XWS/data/comment.go
@@ -1,14 +1,5 @@
 package data
 
-import (
-	"context"
-	"fmt"
-	"time"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type Comment struct {
 	Author  string `json:"username,omitempty"`
 	Content string `json:"text,omitempty"`
@@ -16,22 +7,7 @@ type Comment struct {
 
 type Comments []*Comment
 
-func getCommentByPost(id string) (Comments, error) {
-	postCollection := Client.Database("xws").Collection("posts")
-	ctx, _ := context.WithTimeout(context.Background(), 15*time.Second)
-	docID, err := primitive.ObjectIDFromHex(id)
+func getCommentByCompany(id string) (Comments, error) {
 
-	if err != nil {
-		fmt.Println("[ERROR] can't convert string to ObjectID", err)
-		return nil, ErrPostNotFound
-	}
-
-	var targetPost Post
-	err = postCollection.FindOne(ctx, bson.M{"_id": docID}).Decode(&targetPost)
-	if err != nil {
-		fmt.Println("[ERROR] FindOne() ObjectIDFromHex :", err)
-		return nil, ErrPostNotFound
-	}
-
-	return targetPost.Comments, nil
+	return nil, nil
 }
